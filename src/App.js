b81287a1de4dda69bbec947272dacc309265d080
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-22 14:04:31 -0300

    Base projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Banner from './components/Banner/index.js';
-import Formulario from './components/Formulario/index.js';
 
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      
     </div>
   );
 }
