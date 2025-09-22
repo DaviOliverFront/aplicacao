ab97dad3bdfe57906c60a331bcb82d6af2906381
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-22 15:17:24 -0300

    Finalizando Campotexto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Banner from './components/Banner/index.js';
+import CampoTexto from './components/CampoTexto/index.js';
 
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      
+      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
     </div>
   );
 }
