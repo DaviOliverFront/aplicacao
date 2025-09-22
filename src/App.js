9307aa1d15c5e682ee640ce24c2196b14a9ad07b
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-22 16:06:01 -0300

    formulário 1/2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Banner from './components/Banner/index.js';
-import CampoTexto from './components/CampoTexto/index.js';
-
+import Formulario from './components/Formulario/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Digite seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
+      <Formulario/>
     </div>
   );
 }
