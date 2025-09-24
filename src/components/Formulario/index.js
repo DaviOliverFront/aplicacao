9c0eb7a67eeea0e7795d5fa77919e739041be40b
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-24 18:56:13 -0300

    botão
    Finalizando botão e criando o evento do form.

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,20 +1,27 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/index.js";
 import ListaSuspensa from "../ListaSuspensa/index.js";
+import Botao from "../Botao/index.js";
 
 
 const Formulario = () => {
 
   const times = ["", "Presidência", "Diáconos", "Secretários", "Tesoureiros"];
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido');
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do Membro!</h2>
-         <CampoTexto label="Nome" placeholder="Digite seu nome" />
-         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+         <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+         <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
          <CampoTexto label="Imagem" placeholder="Informe a URL da imagem" />
-         <ListaSuspensa label="Time" itens={times} />
+         <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+         <Botao text="Criar Card" />
       </form>
     </section>
   );
