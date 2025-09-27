90ef7d7a3ad22c5f6484aa0bd4b892840be597d5
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-27 18:03:37 -0300

    Alterando Banner
    Colocando as 3 imagens do Banner para melhorar a responsividade.

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,9 @@ import './Banner.css'
 const Banner = () => {
     return(
         <div className='Banner'>
-            <img src="/images/Banner.png" alt="Banner Principal"/>
+            <img src="/images/peixes.svg" alt="Banner Principal"/>
+            <img src="/images/IBF.svg" alt="Logo da Org"/>
+            <img src='/images/UIFD.svg' alt='Logo da UIFD'/>
         </div>
     )
 }
