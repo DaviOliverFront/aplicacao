9256ae1d008ee975084ebb2a8819139aebe828a8
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-27 14:14:06 -0300

    Finalizando card-colaboradores

---
+++ src/components/Membro/index.js
@@ -0,0 +1,15 @@
+import './Membro.css'
+
+const Membro = () => {
+    return (<div className='Membro'> 
+        <div className='cabecalho'>
+            <img src='https://github.com/DaviOliverFront.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>Davi Oliveira</h4>
+            <h5>Secretário</h5>
+        </div>
+    </div>)
+}
+
+export default Membro;
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,12 @@
+import Membro from '../Membro';
 import './Time.css'
 
 const Time = (props) => {
   return (
     <section className='time' style={{ backgroundColor: props.corSecundaria}}>
         <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
+        <Membro/>
+        <Membro/>
     </section>
   );
 }
